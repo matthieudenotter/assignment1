86805de5179118b394b25f6de81e520ec5dbd2a5
Matthieu den Otter <matthieudenotter@gmail.com>
2023-05-01 21:54:12 +0200

    sync

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 def similarity(set_1, set_2, outfile):
     """
     Calculates list similarities between corresponding lists in set of lists 
-        parameters
-        ----------
-        set_1, set_2: text files with lines, containing a set of intervals [x1,y1],[x2,y2]...[xn,yn]
-    
-        output
-        ------
-        outfile with value mean list similarity between corresponding lists in two sets parameters
+    parameters
+    ----------
+    set_1, set_2: text files with lines, containing a set of intervals [x1,y1],[x2,y2]...[xn,yn]
+
+    output
+    ------
+    outfile with value mean list similarity between corresponding lists in two sets parameters
     """
+    global no_iterations; no_iterations = 0
     if (set1 := read_data_from_file(set_1)) and (set2 := read_data_from_file(set_2)):
         if ((set_length := len(set1)) != len(set2)):
             print('Error: number of sets has to be equal in each file!')
         
-        list_similarity = 0
-        # iterate list symetric similarities between all corresponding lists in sets (i.e. first lists in sets, second lists etc....)
+        list_similarity = 0        
         for i in range(len(set1)):
+        # iterate list symetric similarities between all corresponding lists in sets (i.e. first lists in sets, second lists etc....)
             list_similarity += list_similarity_symetric(set1[i], set2[i])
         # write result to outfile
         outf = open(outfile, 'w')
         outf.write("{:.2f}".format(list_similarity/set_length))
         outf.close()
+        print(no_iterations)
         return True
     else:
         print("No files could not be read")
@@ -37,13 +39,13 @@ def read_data_from_file(filename):
         lines = data.split('\n')
         set_length = len(lines)
         lists = [[]*set_length]*set_length
-        # iterate all lines in set
         for i in range(set_length):
+        # iterate all lines in set
             lines[i] = lines[i].replace("],[","]$[") # introduce seperation character $ between intervals
             lines[i] = lines[i].replace("]",""); lines[i] = lines[i].replace("[","") # remove unnecessary brackets
             lists[i] = lines[i].split("$") # split on new split character
-            # iterate all sets
             for j in range(len(lists[i])):
+            # iterate all sets
                 lists[i][j] = lists[i][j].split(",") # split on comma
                 lists[i][j] = [ int(x) for x in lists[i][j]] # convert all characters to integers
         return(lists)
@@ -64,9 +66,9 @@ def list_similarity(list1, list2):
     Returns asymetric similarity between two lists
     """
     list_similarity = 0
-    # iterate intervals of list1
-    for i in range(len(list1)):
-        # check for overlap in intervals with list
+    for i in range(len(list1)):    
+    # iterate intervals from list1
+    # check for overlap in intervals with list
         if overlap(list1[i],list2):
             list_similarity += 1
     max_list_length = max(len(list1), len(list2))
@@ -75,24 +77,29 @@ def list_similarity(list1, list2):
 def overlap(interval, list):
     """
     Returns boolean overlap=True if there is an overlap in list with interval
+    parameters
+    ----------
+    interval: list with two values(start and stop value of interval). list: list of intervals
+    output
+    ------
+    Boolean: true if overlap
+    algorithm
+    ---------
+    For each interval in list, calculate the sum of the spans of the intervals. If this sum is larger or equals than the joint interval span, there must be an overlap
+    eg: [1,2] and [3,4]. Joint interval: [1:4]. 1+1 < 3 so no overlap
+    eg: [1,2] and [2,4]. Joint interval: [1:4]. 1+2 >= 3  overlap
+    eg: [5,10] and [6,7]. Joint interval: [5:10]. 5+2 >= 5 so overlap
+    eg: [1,2] and [8,9]. Joint interval: [1:9]. 1+1 < 8 so no overlap
     """
-    index_interval = interval[0] # start with first element in interval
     list_length = len(list)
     overlap = False
-    
-    while index_interval <= interval[1] and not overlap:
-        # iterate all values in interval. If overlap found exit loop
-        index_list = 0 # start with first interval in list
-        while index_list < list_length and not overlap and index_interval>list[index_list][0]:
-            # loop over all intervals in list. If overlap found exit loop. 
-            # third condition: exit loop if interval end < start value of interval from list
-            # only possible if intervals are sorted => reduce order n^2
-            index_list_interval = list[index_list][0] # start with start value from interval
-            while index_list_interval <= list[index_list][1] and not overlap:
-                # loop over all values in interval. If overlap found exit loop
-                if index_interval == index_list_interval:
-                    overlap = True
-                index_list_interval += 1
-            index_list += 1
-        index_interval += 1
+    index_list = 0
+    difference_interval = interval[1]-interval[0]
+    while index_list<list_length and not overlap and interval[1] >= list[index_list][0]:
+        print(interval, list[index_list], difference_interval + list[index_list][1] - list[index_list][0] >= max(list[index_list][1],interval[1])-min(list[index_list][0],interval[0]))
+    # iterate intervals in list. Stop if overlap found
+        if difference_interval + list[index_list][1] - list[index_list][0] >= max(list[index_list][1],interval[1])-min(list[index_list][0],interval[0]):
+            overlap = True
+        global no_iterations; no_iterations += 1
+        index_list += 1
     return overlap
